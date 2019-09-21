1cc31c061e7d5e410927eae47f49df8cfa46b80d
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-21 17:13:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +7,50 @@ class Song
     @@all
   end
 
+  def self.create
+    obj = self.new
+    obj.save
+    obj
+  end
+
+  def self.new_by_name (arg)
+    obj = self.new
+    obj.name = arg
+    obj
+  end
+
+  def self.create_by_name (arg)
+    obj = self.new
+    obj.name = arg
+    obj.save
+    obj
+  end
+
+  def self.find_by_name (arg)
+    self.all.find {|obj| obj.name == arg}
+  end
+
+  def self.find_or_create_by_name (arg)
+    if self.find_by_name (arg)
+       self.find_by_name (arg)
+     else
+       self.create_by_name (arg)
+     end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|obj| obj.name}
+  end
+
+  def self.new_from_filename (arg)
+    obj = self.new
+    arg.match(//)
+  end
+
   def save
     self.class.all << self
   end
 
+
 end
+#
